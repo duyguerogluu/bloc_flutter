89d74a997474b0bb66c9cb0d6b42b6629ccea2c5
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-05 00:34:44 +0300

    basic cubit project all extension

--- lib/bloc/bloc_cats_view.dart
+++ lib/bloc/bloc_cats_view.dart
@@ -1,4 +1,8 @@
+import 'package:bloc_flutter/bloc/bloc_cubit.dart';
+import 'package:bloc_flutter/bloc/cats_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
 
 class BlockCatsView extends StatefulWidget {
   const BlockCatsView({super.key});
@@ -10,6 +14,52 @@ class BlockCatsView extends StatefulWidget {
 class _BlockCatsViewState extends State<BlockCatsView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      create: (context) => CatsCubit(),
+      child: buildScaffold(context),
+    );
   }
+
+  Scaffold buildScaffold(BuildContext context) => Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.bloc<CatsCubit>().getCats(); //**** */
+          },
+          child: const Icon(Icons.install_desktop),
+        ),
+        appBar: AppBar(title: const Text("Hello")),
+        body: BlocConsumer<CatsCubit, CatsState>(
+          listener: (context, state) {
+            if (state is CatsError) {
+              //state çalışırken herhangibir and error gelirse
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(state.message),
+                duration: const Duration(seconds: 3),
+              ));
+            }
+          },
+          builder: (context, state) {
+            if (state is CatsInitial) {
+              return const Center(child: Text("hello again"));
+            } else if (state is CatsLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is CatsCompleted) {
+              ListView.builder(
+                itemBuilder: (context, index) => Text(
+                  state.response[index],
+                ),
+                itemCount: state.response.length,
+              );
+            } else {
+              final error = state as CatsError;
+              return Text(error.message);
+            }
+            return const Text("Default condition");
+          },
+        ),
+      );
 }
+
+
--- lib/bloc/bloc_cubit.dart
+++ lib/bloc/bloc_cubit.dart
@@ -2,15 +2,19 @@ import 'package:bloc_flutter/bloc/cats_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CatsCubit extends Cubit<CatsState> {
-  CatsCubit() : super(CatsInitial());
+  CatsCubit() : super(const CatsInitial());
 
-  void getCats() async {
-    emit(const CatsLoading()); //şuan Loadingte olduğunu emit ediyorum
-    await Future.delayed(const Duration(
-        milliseconds:
-            500)); //burda 500 milisaniye bekliyor ve sonra cevabı alıyor
-    final response =
-        Future.value(["a", "b"]); //cevabı aldığı yer burası bir string list
-    emit(CatsCompleted(response)); // artık cevabın geldiğini haber veriyorum.
+  Future<void> getCats() async {
+    try {
+      emit(const CatsLoading()); //şuan Loadingte olduğunu emit ediyorum
+      await Future.delayed(const Duration(
+          milliseconds:
+              500)); //burda 500 milisaniye bekliyor ve sonra cevabı alıyor
+      final response = await Future.value(
+          ["a", "b"]); //cevabı aldığı yer burası bir string list
+      emit(CatsCompleted(response)); // artık cevabın geldiğini haber veriyorum.
+    } catch (e) {
+      emit(const CatsError("Failed to get fetch response"));
+    }
   }
 }
